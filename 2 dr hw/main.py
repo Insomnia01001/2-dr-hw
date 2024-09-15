d02e8a0bc5290b573bd5af007322d9886f31efda
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-09-15 23:25:03 +0300

    Add files via upload

---
+++ 2 dr hw/main.py
@@ -0,0 +1,7 @@
+text1="Mening yoshim -  "
+text2=" Mening ismim - "
+text3="Mening familiyam - "
+text4="Mening manzilim "
+text5 = "bo'sh vaqtimda tel oynayman"
+print(f"{text1}16 , {text2} Muhammadaziz, {text3} Shokirov , {text4} buni rtolmayman")
+print(text5)
